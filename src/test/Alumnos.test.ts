f37b858fbee20cd6f1b5b2b82ca0fd39f2d9fcc3
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-04-14 17:22:04 -0600

    creacion de pruebas unitarias del caso de uso

--- src/test/Alumnos.test.ts
+++ src/test/Alumnos.test.ts
@@ -3,7 +3,7 @@ import { Alumno } from '../core/entities/Alumnos';
 
 describe('Entidad Alumno', () => {
     test('debería crear un alumno con id y nombre', () => {
-        const alumno = new Alumno({ id: '152026', nombre: 'kevin leonardo' });
+        const alumno = new Alumno({ id: '152026', nombre: '123' });
         
         expect(alumno.id).toBe('152026');
         expect(alumno.nombre).toBe('kevin leonardo');
