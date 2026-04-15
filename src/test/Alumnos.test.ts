5663c468e4d3399c464a2237ab32513cd2ac0a96
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-04-15 17:10:06 -0600

    test Alumnos.test.ts

--- src/test/Alumnos.test.ts
+++ src/test/Alumnos.test.ts
@@ -1,11 +1,9 @@
 //prueba unitaria alumnos
 import { Alumno } from '../core/entities/Alumnos';
-
-describe('Entidad Alumno', () => {
-    test('debería crear un alumno con id y nombre', () => {
-        const alumno = new Alumno({ idAlumno: '152026', nombre: '123' });
-        
-        expect(alumno.idAlumno).toBe('152026');
-        expect(alumno.nombre).toBe('kevin leonardo');
-    });
+test('crear un alumno con id y nombre', () => {
+    const alumno = new Alumno({ idAlumno: '152026', nombre: 'Kevin' });
+    
+    console.log('ID del alumno:', alumno.idAlumno);
+    expect(alumno.idAlumno).toBe('152026'); 
+    expect(alumno.nombre).toBe('Kevin');
 });
