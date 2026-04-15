6dc980b46629d88e107b8ada3ff991404248e7af
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-04-15 17:10:34 -0600

    test Monedero.test.ts

--- src/test/Monedero.test.ts
+++ src/test/Monedero.test.ts
@@ -2,7 +2,6 @@
 import { Monedero } from "../core/entities/Monedero";
 
 describe('Pruebas de Nómina Tec-Café', () => {
-    // Creamos la instancia dentro o fuera, pero con el formato de objeto correcto
     const Objmonedero = new Monedero({ idAlumno: '12345', saldo: 100 });
 
     test('recarga debe ser entre 50 y 500', () => { 
