10c861b478216b685df5032fed47ade3d625743b
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-04-15 17:10:58 -0600

    trabajo del main

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,27 @@ async function ejecutar() {
     const useCase = new AgregarMonedero(repo);
 
     const datos = { idAlumno: "ALU-2024", montoInicial: 100 };
+    const datos2 = { idAlumno: "ALU-2023", montoInicial: 100 };
 
     try {
-        console.log("--- Intento 1 ---");
-        await useCase.ejecutar(datos);
-        console.log("Primer monedero creado con éxito.");
+    console.log("--- Intento 1 ---");
+    await useCase.ejecutar(datos);
+    console.log("Primer monedero creado con éxito.");
 
-        console.log("--- Intento 2 (Mismo ID) ---");
-        await useCase.ejecutar(datos); 
-        
-    } catch (error: any) {
-        console.error("ERROR CAPTURADO:", error.message);
+    console.log("--- Intento 2 ---");
+    await useCase.ejecutar(datos2); 
+    console.log("segundo monedero creado con éxito.");
+
+    console.log("--- Intento 3 mismo monedero que el 2 ---");
+    await useCase.ejecutar(datos2); 
+    
+} catch (error: any) {
+    if (error.message.includes("already exists") || error.code === 409) {
+        console.error("AVISO: El monedero ya existe, no se pudo crear por segunda vez.");
+    } else {
+        console.error("ERROR INESPERADO:", error.message);
     }
+}
 }
 
 ejecutar();
